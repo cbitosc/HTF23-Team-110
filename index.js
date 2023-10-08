6bda71cc1c47c3af52bbd5416aa90f7050f96040
nithyanand <pagidinithyanand@gmail.com>
2023-10-08 07:37:19 +0530

    build:

--- index.js
+++ index.js
@@ -1,26 +1,35 @@
+// server.js
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const authRoutes = require("./routes/auth");
+const gardeningTipRoutes = require("./routes/Tips");
+
 const app = express();
-mongoose.connect("mongodb://localhost:27017/sustainable_garden_hub", {
+const PORT = process.env.PORT || 5000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// MongoDB Connection
+mongoose.connect("mongodb://127.0.0.1/sustainable_garden_hub", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
+
 const db = mongoose.connection;
+
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
 db.once("open", () => {
   console.log("Connected to MongoDB");
 });
-// Middleware
-app.use(cors());
-app.use(express.json());
-// MongoDB Connection
-mongoose.connect("mongodb://localhost:27017/sustainable_garden_hub", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-app.use("/api/tips", require("./routes/Tips"));
+
+// Routes
+app.use("/api/auth", authRoutes);
+app.use("/api/gardening-tips", gardeningTipRoutes);
+
 // Start server
-app.listen(5000, () => {
-  console.log(`Server is running on port 5000`);
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
